6e5b556f677e868d56349340e618d768e1f176ef
kritika <patel10kritika@gmail.com>
2020-10-24 08:07:41 +0530

    initial commit

---
+++ src/HotStarComponent/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <Fragment>
+                <h1>Welcome to HotStar</h1>
+            </Fragment>
+         );
+    }
+}
+ 
+export default App;
